ca3d68fc740771e6c262b3fc3e987487607dbb83
Andres Work <andresperezfirigua@gmail.com>
2022-09-27 14:20:36 -0500

    Changed console message

--- DebuggingPractice/DebuggingPractice/Program.cs
+++ DebuggingPractice/DebuggingPractice/Program.cs
@@ -6,7 +6,7 @@ namespace DebuggingPractice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Andres!");
         }
     }
 }
