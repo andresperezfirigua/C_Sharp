7b0ddd636873a0801c97f202d797de722db603a8
Andres Work <andresperezfirigua@gmail.com>
2022-10-05 17:08:41 -0500

    Adding a new class to Inheritance project

---
+++ InheritancePractice/InheritancePractice/Cat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InheritancePractice
+{
+    internal class Cat : Animal
+    {
+        public bool IsResting { get; set; }
+        public Cat (string name, int age) : base (name, age)
+        {
+            IsResting = true;
+        }
+
+
+    }
+}
